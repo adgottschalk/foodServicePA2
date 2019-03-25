116d38580fa599298fdc3bb2b48983385106a0f5
Adam Gottschalk <gottchalk33@gmail.com>
2019-03-24 22:16:21 -0500

    update

--- crm/views.py
+++ crm/views.py
@@ -5,8 +5,23 @@ from .forms import *
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.db.models import Sum
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomerSerializer
+
+
+class CustomerList(APIView):
+
+    def get(self,request):
+        customers_json = Customer.objects.all()
+        serializer = CustomerSerializer(customers_json,many=True)
+        return Response(serializer.data)
+
 
 now = timezone.now()
+
+
 def home(request):
     return render(request, 'crm/home.html',
                   {'crm':home})
